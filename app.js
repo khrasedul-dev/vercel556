ae4b7b245d1c9c1b59ef889a2c8c0e6a70f07d94
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-11-07 13:24:27 +0600

    update

---
+++ app.js
@@ -0,0 +1,46 @@
+const {Telegraf} = require('telegraf')
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const shecma = new mongoose.Schema({
+    msg: {
+        type: String
+    }
+})
+const testmodel = mongoose.model('test',shecma)
+
+
+mongoose.connect('mongodb+srv://rasedul20:rasedul20@telegramcluster.xfaz1rx.mongodb.net/sdBot', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+
+
+const app = express()
+
+const bot = new Telegraf(process.env.TOKEN)
+
+
+bot.start(ctx=>{
+    ctx.reply('bot start')
+})
+
+bot.command('testdb',ctx=>{
+    testmodel.find()
+    .then(data=>{
+        ctx.reply(data[0].msg)
+    })
+    .catch(e=>console.log(e))
+})
+
+app.use(bot.webhookCallback(`/`))
+
+
+app.get('/',(req,res)=>{
+    res.json({"status":"The bot is running"})
+})
+
+app.listen(5000,()=>{
+    console.log("The bot is running")
+})
